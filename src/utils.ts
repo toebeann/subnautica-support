53cff38783241257be2d463e409bca419cd69da4
Tobey Blaber <contact@tobey.me>
2023-01-06 07:10:32 +0000

    Added utility function to get installed mods

--- src/utils.ts
+++ src/utils.ts
@@ -37,10 +37,10 @@ export const getUnityVersion = async (gamePath: string): Promise<string> => {
 }
 
 /**
- * Utility function to retrieve the Subnautica game discovery result from the Vortex API.
+ * Utility function to retrieve a game discovery result from the Vortex API.
  * @param api 
- * @param gameId 
- * @returns The Subnautica game discovery result, or undefined if the game has not been discovered.
+ * @param gameId The game ID to retrieve the discovery result for. Defaults to Subnautica.
+ * @returns The game discovery result, or undefined if the game has not been discovered.
  */
 export const getDiscovery = (api: IExtensionApi, gameId: string = NEXUS_GAME_ID): IDiscoveryResult | undefined =>
     api.getState().settings.gameMode.discovered[gameId];
@@ -51,3 +51,18 @@ export const getDiscovery = (api: IExtensionApi, gameId: string = NEXUS_GAME_ID)
  * @returns The path to the mods directory. If the current beta branch is unknown, the path to the BepInEx plugins directory is returned.
  */
 export const getModPath = (gamePath: string): string => join(gamePath, store('branch') === 'legacy' ? QMM_MOD_PATH : BEPINEX_MOD_PATH);
+
+/**
+ * Utility function to retrieve a list of mods for the specified game from the Vortex API.
+ * @param api 
+ * @param onlyInstalled Whether to filter the list to only installed mods.
+ * @param gameId The game ID to retrieve the mods for. Defaults to Subnautica.
+ * @returns A list of mods for the specified game.
+ */
+export const getMods = (api: IExtensionApi, onlyInstalled = false, gameId: string = NEXUS_GAME_ID) => {
+    const mods = Object.values(api.getState().persistent.mods[gameId] ?? {});
+
+    return onlyInstalled
+        ? mods.filter(mod => mod.state === 'installed')
+        : mods;
+}
