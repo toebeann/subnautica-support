0cb9f72f61cd1d281a1d7958d9145ab654c4deb3
Tobey Blaber <contact@tobey.me>
2023-01-12 15:14:56 +0000

    Added installers and modtypes for legacy mods

---
+++ src/mod-types/customposters-pack.ts
@@ -0,0 +1,61 @@
+import { basename, dirname, sep } from 'path';
+import { getDiscovery, getModPath } from '../utils';
+import { CUSTOMPOSTERS_FOLDER, MRPURPLE6411_ADDON_FILES, MRPURPLE6411_ADDON_MANIFEST } from '../installers/mrpurple6411-addon-pack';
+import { NEXUS_GAME_ID } from '../platforms/nexus';
+import { IExtensionApi, IExtensionContext, IGame, IInstruction } from 'vortex-api/lib/types/api';
+
+/**
+ * QModManager Mod mod type.
+ */
+export const CUSTOMPOSTERS_PACK_MOD_TYPE = 'customposters-pack';
+
+/**
+ * Determines whether the mod type is supported for the specified game.
+ * @param gameId 
+ * @returns 
+ */
+export const isSupported = (gameId: string): boolean => gameId === NEXUS_GAME_ID;
+
+/**
+ * Retrieves the absolute path to the installation directory for this mod type.
+ * @param api 
+ * @param game 
+ * @returns 
+ */
+export const getPath = (api: IExtensionApi, game: IGame): string => getModPath(getDiscovery(api, game.id)?.path);
+
+/**
+ * Determines whether a given mod is of this mod type.
+ * @returns 
+ */
+export const test = async (installInstructions: IInstruction[]): Promise<boolean> => {
+    const copy = installInstructions.filter(instruction => instruction.type === 'copy');
+    const copyDestinationsLowerCase = copy.filter(instruction => instruction.destination)
+        .map(instruction => instruction.destination!.toLowerCase());
+    const manifests = copyDestinationsLowerCase.filter(dest => basename(dest) === MRPURPLE6411_ADDON_MANIFEST.toLowerCase());
+    const rootDir = basename(dirname(dirname(manifests[0] ?? '')));
+    const destinationDirs = copyDestinationsLowerCase.map(dest => dirname(dest).split(sep));
+    const index = destinationDirs[0]?.indexOf(rootDir);
+    return rootDir.toLowerCase() === CUSTOMPOSTERS_FOLDER.toLowerCase()
+        && destinationDirs.every(segments => segments.indexOf(rootDir.toLowerCase()) === index)
+        && MRPURPLE6411_ADDON_FILES.every(file => copyDestinationsLowerCase.map(f => basename(f)).includes(file.toLowerCase()));
+}
+
+/**
+ * Registers the CustomPosters Pack mod type with the Vortex API.
+ * @param context 
+ * @returns 
+ */
+export const register = (context: IExtensionContext) =>
+    context.registerModType(
+        CUSTOMPOSTERS_PACK_MOD_TYPE,
+        90,
+        isSupported,
+        (game: IGame) => getPath(context.api, game),
+        test,
+        {
+            name: 'CustomPosters Pack',
+            mergeMods: true
+        }
+    );
+export default register;
