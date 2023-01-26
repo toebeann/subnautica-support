d068e939de90dea7273c55f2db3970a752278cbe
Tobey Blaber <contact@tobey.me>
2023-01-26 00:08:53 +0000

    Enforce bepinex-plugin modtype when not on legacy branch and mod assemblies contain at least one class marked with the BepInPlugin attribute

--- src/installers/bepinex-plugin.ts
+++ src/installers/bepinex-plugin.ts
@@ -1,8 +1,13 @@
+import { execFile } from 'child_process';
 import { basename, dirname, extname, join, sep } from 'path';
 import { BEPINEX_INJECTOR_CORE_FILES } from './bepinex';
 import { BEPINEX_CORE_DIR, BEPINEX_DIR, BEPINEX_PLUGINS_DIR } from '../bepinex';
+import { getDiscovery } from '../utils';
+import { BEPINEX_PLUGIN_MOD_TYPE } from '../mod-types/bepinex-plugin';
+import { QMM_MOD_MANIFEST } from '../mod-types/qmodmanager-mod';
 import { NEXUS_GAME_ID } from '../platforms/nexus';
-import { IExtensionContext, IInstallResult, IInstruction, TestSupported } from 'vortex-api/lib/types/api';
+import { getBranch } from '../platforms/steam';
+import { IDiscoveryResult, IExtensionApi, IExtensionContext, IInstallResult, IInstruction, TestSupported } from 'vortex-api/lib/types/api';
 
 /**
  * Determines whether the installer is supported for the given mod files and game.
@@ -29,7 +34,7 @@ export const testSupported: TestSupported = async (files, gameId) => {
  * @param files 
  * @returns 
  */
-export const install = async (files: string[]): Promise<IInstallResult> => {
+export const install = async (api: IExtensionApi, files: string[], workingPath: string): Promise<IInstallResult> => {
     const sansDirectories = files.filter(file => !file.endsWith(sep));
     const assembly = sansDirectories.find(file => extname(file).toLowerCase() === '.dll')!;
     const assemblyDir = basename(dirname(assembly));
@@ -37,16 +42,68 @@ export const install = async (files: string[]): Promise<IInstallResult> => {
     const filtered = sansDirectories.filter(file => file.split(sep).indexOf(assemblyDir) === assemblyDirIndex);
     const index = assembly.split(sep).indexOf(BEPINEX_PLUGINS_DIR);
 
-    return {
-        instructions: [
-            ...filtered.map((source): IInstruction => {
-                return {
-                    type: 'copy',
-                    source,
-                    destination: join(dirname(source).split(sep).slice(index + 1).join(sep), basename(source)),
-                }
-            })
-        ]
+    const instructions = filtered.map((source): IInstruction => {
+        return {
+            type: 'copy',
+            source,
+            destination: join(dirname(source).split(sep).slice(index + 1).join(sep), basename(source)),
+        }
+    });
+
+    // if the mod contains a QModManager manifest but we're not on the legacy branch,
+    // determine whether the mod contains BepInEx plugins and set the mod type accordingly
+    // to handle the case where a mod can be installed either as a QModManager mod or a BepInEx plugin
+    if (sansDirectories.some(f => basename(f).toLowerCase() === QMM_MOD_MANIFEST)
+        && await getBranch(api) !== 'legacy'
+        && await hasBepInExPlugins(api, files, workingPath)) {
+        instructions.push({
+            type: 'setmodtype',
+            value: BEPINEX_PLUGIN_MOD_TYPE
+        });
+    }
+
+    return { instructions };
+}
+
+/**
+ * A helper utility to determine whether the given mod files contain BepInEx plugins via the BepInEx.AssemblyInspection.Console.exe command-line utility.
+ * @param api 
+ * @param files 
+ * @param workingPath 
+ * @param discovery 
+ * @returns 
+ */
+const hasBepInExPlugins = async (api: IExtensionApi, files: string[], workingPath: string, discovery: IDiscoveryResult | undefined = getDiscovery(api)) => {
+    if (!discovery?.path) return false;
+
+    const sansDirectories = files.filter(file => !file.endsWith(sep));
+    const assemblies = sansDirectories.filter(file => extname(file).toLowerCase() === '.dll'.toLowerCase());
+    const managedPath = join(discovery.path, 'Subnautica_Data', 'Managed');
+
+    try {
+        for (const assembly of assemblies) {
+            if (await new Promise<boolean>((resolve, reject) => {
+                execFile(join(api.extension!.path, 'BepInEx.AssemblyInspection.Console.exe'),
+                    [
+                        '-t', 'Plugins',
+                        '-f', JSON.stringify(join(workingPath, assembly)),
+                        '-s', JSON.stringify(managedPath), JSON.stringify(join(discovery.path!, BEPINEX_DIR, BEPINEX_CORE_DIR)),
+                    ], { windowsHide: true },
+                    (error, stdout) => {
+                        if (error) {
+                            reject(error);
+                        } else {
+                            try {
+                                resolve(JSON.parse(stdout.trim()));
+                            } catch {
+                                resolve(false);
+                            }
+                        }
+                    });
+            })) return true;
+        }
+    } catch {
+        return false;
     }
 }
 
@@ -55,6 +112,6 @@ export const install = async (files: string[]): Promise<IInstallResult> => {
  * @param context 
  * @returns 
  */
-export const register = (context: IExtensionContext) => context.registerInstaller('bepinex-plugin', 35, testSupported, install);
+export const register = (context: IExtensionContext) => context.registerInstaller('bepinex-plugin', 35, testSupported, (files, workingPath) => install(context.api, files, workingPath));
 export default register;
 
