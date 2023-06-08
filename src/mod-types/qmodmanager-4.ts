2fd2cdfc2dd32b0809af01da8b059b1d2da1c591
Tobey Blaber <contact@tobey.me>
2023-06-08 09:00:10 +0100

    Fix BepInEx\* being installed to root game dir

--- src/mod-types/qmodmanager-4.ts
+++ src/mod-types/qmodmanager-4.ts
@@ -16,13 +16,15 @@
  * this program; if not, see <https://www.gnu.org/licenses>.
  */
 import { join } from 'path';
-import { getPath, isSupported } from './bepinex-5';
-import { BEPINEX_PLUGINS_DIR } from '../bepinex';
+import { isSupported } from './bepinex-5';
+import { BEPINEX_DIR, BEPINEX_PLUGINS_DIR } from '../bepinex';
 import { QMM_DIR } from '../qmodmanager';
+import { getDiscovery } from '../utils';
 import { types } from 'vortex-api';
 import IExtensionContext = types.IExtensionContext;
 import IGame = types.IGame;
 import IInstruction = types.IInstruction;
+import IState = types.IState;
 
 /**
  * QModManager 4 mod type.
@@ -33,6 +35,14 @@ export const QMM_4_MOD_TYPE = 'qmodmanager-4';
  * QModManager core filename.
  */
 export const QMM_CORE_DLL = 'QModInstaller.dll';
+
+/**
+ * Retrieves the absolute path to the installation directory for this mod type.
+ * @param state 
+ * @param game 
+ * @returns 
+ */
+export const getPath = (state: IState, game: IGame): string => join(getDiscovery(state, game.id)?.path ?? '', BEPINEX_DIR);
 
 /**
  * Determines whether a given mod is of this mod type.
